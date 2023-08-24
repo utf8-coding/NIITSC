8058ffa0efb6044c849492841a38cc23539b2b6c
utf8-coding <1422346447@qq.com>
2023-08-24 18:08:38 +0800

    空工程，纯空

---
+++ NIITSC_starter_pack/USER/variables.c
@@ -0,0 +1,4 @@
+#include "stm32f4xx.h"                  // Device header
+#include "variables.h"
+
+uint8_t flag_start = 0;
---
+++ NIITSC_starter_pack/USER/variables.h
@@ -0,0 +1,8 @@
+//Global flags and variable/constants.
+#ifndef __H_VARIABLES
+#define __H_VARIABLES
+#include "stdint.h"
+
+extern uint8_t flag_start;
+
+#endif
