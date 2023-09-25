5da1cf3a04f3d9fe2948023428ba1fc416675ae0
blue <143619954+Han-777@users.noreply.github.com>
2023-09-25 19:56:20 +0800

    main.c

--- NIITSC_code/USER/main.c
+++ NIITSC_code/USER/main.c
@@ -33,7 +33,7 @@ int main(void)
 	Encoder_Tim_Config(1000, 8400); // 84MHz/8400/1000 = 10Hz
 //	OPENMV_USART_Config();
 	QRCODE_UART_Config();
-	SCREEN_USART_Config();
+	Screen_Init();
 	SERVO_USART_Config();
 	Moto_Init(18 - 1, 8400 -1);
 	Laser_Init();
